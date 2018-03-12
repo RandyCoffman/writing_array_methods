d997f0aefa3c9d43fa9222a211156512f85d4166
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-11 20:47:22 -0400

    all tests work as expected

--- putting_element_into_the_last_index_test.rb
+++ putting_element_into_the_last_index_test.rb
@@ -13,4 +13,16 @@ class Push_method < Minitest::Test
 	def test_push
 		assert_equal(["1", "2", "3", "4", "5"], push("5"))
 	end
+
+	def test_push2
+		assert_equal(["1", "2", "3", "4", "6"], push("6"))
+	end
+
+	def test_push3
+		assert_equal(["1", "2", "3", "4", "7"], push("7"))
+	end
+
+	def test_push4
+		assert_equal(["1", "2", "3", "4", "8"], push("8"))
+	end
 end
