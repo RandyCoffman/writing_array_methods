91e774f3fed7864eb1bed279aca877bf1a327b1a
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-11 20:15:08 -0400

    final tests done and passing

--- putting_element_into_the_first_index_test.rb
+++ putting_element_into_the_first_index_test.rb
@@ -13,4 +13,16 @@ class Unshift_method < Minitest::Test
 	def test_unshift
 		assert_equal(["0", "1", "2", "3", "4"], unshift("0"))
 	end
+
+	def test_unshift2
+		assert_equal(["a", "1", "2", "3", "4"], unshift("a"))
+	end
+
+	def test_unshift3
+		assert_equal(["b", "1", "2", "3", "4"], unshift("b"))
+	end
+
+	def test_unshift4
+		assert_equal(["c", "1", "2", "3", "4"], unshift("c"))
+	end
 end
